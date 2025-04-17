8c6f2c5548598d9f55167e708e8341b619a94141
Jonathan Mendoza <john.2114@hotmail.com>
2025-04-17 18:13:51 +0200

    OOP LECTURE version: 0.1 added

---
+++ Object-oriented-programming/Car.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_oriented_programming
+{
+    class Car
+    {
+        //Property
+        private string _model = "";
+        private string _brand= "";
+        private bool _isLuxury;
+
+        // Getter and Setter with lambda expression 
+        public string Model { get => _model; set => _model = value; }
+       
+        
+        public string Brand
+        {
+            get
+            {
+                if (_isLuxury)
+                {
+                    return _brand + "- Luxury Edition";
+                }
+                else
+                {
+                    return _brand;
+                }
+            }
+
+            set
+            {
+                /* Returns true if the string is null or empty ("").
+                 Check if a string has no value or is empty.*/
+
+                if (string.IsNullOrEmpty(value))
+                {
+
+                    Console.WriteLine("You entered NOTHING");
+                    _brand = "DEFAULTVALUE";
+                }
+                else
+                {
+                    _brand = value;
+                }
+
+            }
+        }
+
+        public bool IsLuxury { get => _isLuxury; set => _isLuxury = value; }
+
+        //Custom Construtor
+        public Car(string model, string brand, bool isLuxury)
+        {
+            Model = model;
+            Brand = brand;
+            IsLuxury = isLuxury;
+            
+            Console.WriteLine($"A {Brand} of the" + $" model {Model} has been created");
+        }
+
+
+
+        public void Drive()
+        {
+            Console.WriteLine($"I am a Model: {Model} and i am Driving");
+        }
+
+
+    }
+}
---
+++ Object-oriented-programming/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Object_oriented_programming;
+//125
+class Program
+{
+    static void Main(string[] args)
+    {
+        CreateCustomer();
+    }
+
+
+    // In the below Method, are many combinations
+   public static void CreateCustomer()
+    {
+        Customer earl = new Customer("Earl");
+        // var name = earl.Name;
+
+        Customer frank = new Customer("Frank" , "MainStreet1" , "885412");
+        // var name2 = frank.Name;
+
+        // Console.WriteLine("Name of Customer is: " + name);
+
+        Customer myCustomer = new Customer();
+        //Console.WriteLine("Please enter the customers Name: ");
+        //myCustomer.Name = Console.ReadLine();
+
+        myCustomer.SetDetails("Lisa", "Am Street 2", "885525");
+        
+       // Console.WriteLine("Details about customer: " + myCustomer.Name);
+
+        Console.WriteLine("My Customer is: " + myCustomer.Name + " and lives: " +myCustomer.Address);
+        Console.WriteLine("Earl is: " + earl.Name + " and lives: " +earl.Address);
+
+
+        Console.ReadKey();
+
+
+
+
+    }
+
+
+
+
+    public static void CreateAuto()
+    {
+
+        Car audi = new Car("A3", "Audi", false);
+        Car bmw = new Car("I7", "BWM", true);
+
+       //Console.WriteLine("Please enter the Brand name: ");
+
+        //*** Setting Brand ** 
+        
+        //audi.Brand = Console.ReadLine();
+
+        // *** Getting Brand **
+
+       // Console.WriteLine("Brand is: " + audi.Brand);
+       // Console.WriteLine("Brand is: " + bwm.Brand);
+
+        audi.Drive();
+        bmw.Drive();
+
+        Console.ReadKey();
+
+
+    }
+
+
+
+
+
+
+
+}
